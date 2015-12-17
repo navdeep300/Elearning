1a14078566355371ac836a7e3553cf0e4e7fe593
Navdeep Singh <navdeepsaini48@gmail.com>
2015-12-17 19:45:10 +0530

    Developers Info

---
+++ developers.php
@@ -0,0 +1,47 @@
+<?php include('header_dashboard.php'); ?>
+    <body id="class_div">
+		<?php include('navbar_about.php'); ?>
+        <div class="container-fluid">
+            <div class="row-fluid">
+                <div class="span12" id="content">
+                     <div class="row-fluid">
+                        <!-- block -->
+                        <div class="block">
+								<div class="navbar navbar-inner block-header">
+									<div id="" class="muted pull-right"><a href="index.php"><i class="icon-arrow-left"></i> Back</a></div>
+								</div>
+                            <div class="block-content collapse in">
+							<h3 align="center">Developers</h3>
+							<hr>
+                                <div class="span3">
+										<center>
+										<img id="developers" src="admin/images/navi.jpg" class="img-circle">
+										<hr>
+										<p>Name: Navdeep Singh</p>
+										<p>Address: Ludhiana City</p>
+										<p>Email: navdeepsaini48@gmail.com</p>
+										<p>Position: Programmer</p>
+										</center>
+								</div>
+                                <div style="float:right"class="span3">
+								
+								<img id="developers" src="admin/images/guri.jpg" class="img-circle">
+								<hr>
+										<p>Name: Gurjot Pal Singh</p>
+										<p>Address: Ludhiana City</p>
+										<p>Email: guri.singh346@gmail.com</p>
+										<p>Position: Programmer</p>
+								
+								</div>
+                            
+                            </div>
+                        </div>
+                        <!-- /block -->
+                    </div>
+                </div>
+            </div>
+		<?php include('footer.php'); ?>
+        </div>
+		<?php include('script.php'); ?>
+    </body>
+</html>
