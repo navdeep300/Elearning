f1a5eea13a46c1c5d9ecb02694bd3e0e13d16c1c
Navdeep Singh <navdeepsaini48@gmail.com>
2015-12-24 15:17:33 +0530

    index title page

---
+++ title_index.php
@@ -0,0 +1,38 @@
+				
+								<div class="row-fluid">
+
+						<div class="span12">
+						
+						</div>	
+													
+							</div>
+							<div class="row-fluid">
+
+						<div class="span4">
+						<img class="index_logo" src="admin/images/Logo.png">
+						</div>	
+						<div class="span8">
+						
+								<div class="title">
+							<p class="chmsc">Guru Nanak Dev Engineering College- Ludhiana</p>
+							<h3>
+
+							<p> E Learning</p>
+						
+							</h3>		
+						</div>
+			
+						</div>							
+							</div>
+				
+				<div class="row-fluid">
+
+						<div class="span12">
+						<br>
+								<div class="motto">
+												<p>GNDEC EXCELE:</p>
+												<p>Excellence, Competence and Educational</p>
+												<p>Leadership in Engineering and Technology</p>
+								</div>		
+						</div>		
+				</div>
